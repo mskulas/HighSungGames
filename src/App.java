6defe2ae0c3afb40e8193ecc40d121e946e1a729
mskulas <mskulas@murraystate.edu>
2025-12-04 17:10:52 -0600

    Finally added code to end the game

--- src/App.java
+++ src/App.java
@@ -1,34 +1,35 @@
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class App {
+    // volatile so changes are visible across threads immediately
+    public static volatile boolean gameOver = false;
+    
     public class Knight extends Thread {
+        // Knight's score - must be 1000 to win
         int kScore = 0;
-        public Wizard wizard;
-        public void setWizard(Wizard wiz) {
-            this.wizard = wiz;
-        }
-        public void setback (int amount) {
-            kScore -= amount;
-            if (kScore < 0) kScore = 0;
-        }
         public boolean isDone() {
             return Thread.currentThread().isInterrupted();
         }
         public void run() {
             System.out.println("Knight is on a quest to catch the thief!");
-            Random d20 = new Random();
+            Random rng = new Random();
             try {
-                while (kScore < 100) {
-                    int roll = d20.nextInt(1, 20);
-                    System.out.println("Knight rolls a d20 and gets: " + roll);
-                    Thread.sleep(500);
+                while (!gameOver && kScore < 1000) {
+                    int roll = rng.nextInt(1, 20);
+                    System.out.println("Knight rolls a rng and gets: " + roll);
                     kScore += roll;
                     System.out.println("The Knight is " + kScore + "% of the way to catching the Thief.");
-                    wizard.setback(roll / 2);
-                    Thread.sleep(500);
+                    // if we reached the goal, set the global flag and break
+                    if (kScore >= 1000) {
+                        if (!gameOver) {
+                            System.out.println("The Knight has caught the Thief!");
+                            gameOver = true;
+                        }
+                        break;
+                    }
+                    Thread.sleep(rng.nextInt(500, 1000));
                 }
-                System.out.println("The Knight has caught the Thief!");
-                this.interrupt();
             }
             catch (InterruptedException e) {
                 System.out.println("Knight's quest has ended.");
@@ -37,32 +38,27 @@ public class App {
     }
     public class Thief extends Thread {
         int tScore = 0;
-        public Knight knight;
-        public void setKnight(Knight kni) {
-            this.knight = kni;
-        }
-        public void setback (int amount) {
-            tScore -= amount;
-            if (tScore < 0) tScore = 0;
-        }
         public boolean isDone() {
             return Thread.currentThread().isInterrupted();
         }
         public void run() {
             System.out.println("Thief is on a quest to steal from the Wizard!");
-            Random d20 = new Random();
+            Random rng = new Random();
             try {
-                while (tScore < 100) {
-                    int roll = d20.nextInt(1, 20);
-                    System.out.println("Thief rolls a d20 and gets: " + roll);
-                    Thread.sleep(500);
+                while (!gameOver && tScore < 1000) {
+                    int roll = rng.nextInt(1, 20);
+                    System.out.println("Thief rolls a rng and gets: " + roll);
                     tScore += roll;
                     System.out.println("The Thief is " + tScore + "% of the way to robbing the Wizard.");
-                    knight.setback(roll / 2);
-                    Thread.sleep(500);
+                    if (tScore >= 1000) {
+                        if (!gameOver) {
+                            System.out.println("The Thief has secured his plunder!");
+                            gameOver = true;
+                        }
+                        break;
+                    }
+                    Thread.sleep(rng.nextInt(500, 1000));
                 }
-                System.out.println("The Thief has secured his plunder!");
-                this.interrupt();
             }
             catch (InterruptedException e) {
                 System.out.println("Thief's quest has ended.");
@@ -71,32 +67,27 @@ public class App {
     }
     public class Wizard extends Thread {
         int wScore = 0;
-        public Thief thief;
-        public void setThief(Thief thi) {
-            this.thief = thi;
-        }
-        public void setback (int amount) {
-            wScore -= amount;
-            if (wScore < 0) wScore = 0;
-        }
         public boolean isDone() {
             return Thread.currentThread().isInterrupted();
         }
         public void run() {
-            System.out.println("Wizard is on a quest to vaniquish the Knight!");
-            Random d20 = new Random();
+            System.out.println("Wizard is on a quest to vanquish the Knight!");
+            Random rng = new Random();
             try {
-                while (wScore < 100) {
-                    int roll = d20.nextInt(1, 20);
-                    System.out.println("Wizard rolls a d20 and gets: " + roll);
-                    Thread.sleep(500);
+                while (!gameOver && wScore < 1000) {
+                    int roll = rng.nextInt(1, 20);
+                    System.out.println("Wizard rolls a rng and gets: " + roll);
                     wScore += roll;
                     System.out.println("The Wizard is " + wScore + "% of the way to vanquishing the Knight.");
-                    thief.setback(roll / 2);
-                    Thread.sleep(500);
+                    if (wScore >= 1000) {
+                        if (!gameOver) {
+                            System.out.println("The Wizard has eliminated the Knight!");
+                            gameOver = true;
+                        }
+                        break;
+                    }
+                    Thread.sleep(rng.nextInt(500, 1000));
                 }
-                System.out.println("The Wizard has eliminated the Knight!");
-                this.interrupt();
             }
             catch (InterruptedException e) {
                 System.out.println("Wizard's quest has ended.");
@@ -107,25 +98,32 @@ public class App {
         Knight knight = new App().new Knight();
         Thief thief = new App().new Thief();
         Wizard wizard = new App().new Wizard();
-        knight.setWizard(wizard);
-        thief.setKnight(knight);
-        wizard.setThief(thief);
 
         knight.start();
         thief.start();
         wizard.start();
 
+        // Wait until a thread sets gameOver. Sleep briefly in the loop to avoid busy-waiting.
+        while (!gameOver) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                // ignore and re-check
+            }
+        }
+
+        // interrupt other threads so they wake from sleep and exit promptly
+        knight.interrupt();
+        thief.interrupt();
+        wizard.interrupt();
+
+        // wait for threads to finish before exiting
         try {
             knight.join();
             thief.join();
             wizard.join();
-            if (knight.isDone() || thief.isDone() || wizard.isDone()) {
-                knight.interrupt();
-                thief.interrupt();
-                wizard.interrupt();
-            }
         } catch (InterruptedException e) {
-            System.out.println("Main thread interrupted.");
+            // if main is interrupted, just exit
         }
     }
 }
